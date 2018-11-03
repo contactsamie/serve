f698a42c8daac4a2b579a9a98cbdfbdd2b953086
Sam 塞缪尔 <contactsamie@users.noreply.github.com>
2018-11-02 20:32:10 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    using System.Diagnostics;
+    using System.IO;
+    using System.Net;
+    using System.Net.Mime;
+    using System.Net.Sockets;
+    using System.Reflection;
+    using System.Threading;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var loc = Assembly.GetEntryAssembly().Location;
+            //create server with auto assigned port
+            var myServer = new SimpleHttpServer(System.IO.Path.GetDirectoryName(loc));
+            Process.Start("http://localhost:" + myServer.Port);
+            Console.ReadKey();
+        }
+    }
+  
+}
